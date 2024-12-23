7b2cd51671086a00b76fe26e0e7372dbe09cfdea
NathanGRSDesktop <nathangeers00@gmail.com>
2024-12-23 13:46:29 +0100

    Initial Commit

---
+++ src/main/java/com/nathan/javafxperiode2faopdrenergiecurrent/Customer.java
@@ -0,0 +1,40 @@
+package com.nathan.javafxperiode2faopdrenergiecurrent;
+
+public class Customer {
+    private int customerID;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerAdvance;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerAdvance() {
+        return customerAdvance;
+    }
+
+    public void setCustomerAdvance(String customerAdvance) {
+        this.customerAdvance = customerAdvance;
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.nathan.javafxperiode2faopdrenergiecurrent {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.nathan.javafxperiode2faopdrenergiecurrent to javafx.fxml;
+    exports com.nathan.javafxperiode2faopdrenergiecurrent;
+}
