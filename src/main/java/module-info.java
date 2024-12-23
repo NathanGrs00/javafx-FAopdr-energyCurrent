828e2073620f8d03c2c5ed97c8860f5cb28ffa43
NathanGRSDesktop <nathangeers00@gmail.com>
2024-12-23 13:47:50 +0100

    Revert "Initial Commit"
    This reverts commit 7b2cd51671086a00b76fe26e0e7372dbe09cfdea.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.nathan.javafxperiode2faopdrenergiecurrent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.nathan.javafxperiode2faopdrenergiecurrent to javafx.fxml;
-    exports com.nathan.javafxperiode2faopdrenergiecurrent;
-}
